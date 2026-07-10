6ffbe3f10478a46a5530166164a9ff7644ec7f70
bunny <banhibikashsaha@gmail.com>
2026-07-10 19:26:26 +0530

    Added config data

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -5,9 +5,17 @@ namespace WebApplication1.Controllers
     [Route("/")]
     public class HomeController : Controller
     {
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [HttpGet("/")]
         public IActionResult Index()
         {
+            ViewBag.MyKey = _configuration["MyKey"];
             //throw new InvalidOperationException();
             return View();
         }
