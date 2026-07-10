25b3da96820bef52075b27f4ca53434d14f7e35f
bunny <banhibikashsaha@gmail.com>
2026-07-10 19:58:49 +0530

    added config data:3

--- WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2df82aa25c4d3ffba8d49c88f430eb13c41bb1c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c5c343156d634395b67e0ebac2d5b80c04cbe91")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
